e3d6e9195bce973923d322cd1f68e1d5faee218f
finn <git@printf.win>
2020-03-21 14:31:16 +0800

    format text

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,33 +1,33 @@
 class LinksController < ApplicationController
-	def new
-		@new_link = Link.new
-	end
+  def new
+    @new_link = Link.new
+  end
 
-	def create
-		@new_link = Link.new(link_params)
+  def create
+    @new_link = Link.new(link_params)
 
-		if @new_link.save
-			redirect_to @new_link
-		else
-			render 'new'
-		end
-	end
+    if @new_link.save
+      redirect_to @new_link
+    else
+      render 'new'
+    end
+  end
 
-	def show
-		@link_id = Link.find(params[:id])
-	end
+  def show
+    @link_id = Link.find(params[:id])
+  end
 
-	def shortened_link_redirect
-		@link = Link.new
-		shortened_link = params[:shortened_link]
-		ss = @link.get_full_url(shortened_link: shortened_link)
-		puts "ss: #{ss}"
-		redirect_to ss
-	end
+  def shortened_link_redirect
+    @link = Link.new
+    shortened_link = params[:shortened_link]
+    ss = @link.get_full_url(shortened_link: shortened_link)
+    puts "ss: #{ss}"
+    redirect_to ss
+  end
 
-	private
-		def link_params
-	    params.require(:link).permit(:full_link)
-	  end
+  private
+    def link_params
+      params.require(:link).permit(:full_link)
+    end
 
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,58 +1,58 @@
 class Link < ApplicationRecord
-	validates :full_link, presence: true
-	after_create :update_link_with_shortened_link
-	after_validation :smart_add_url_protocol
+  validates :full_link, presence: true
+  after_create :update_link_with_shortened_link
+  after_validation :smart_add_url_protocol
 
 
-	def get_full_url(shortened_link:)
-		self.recover_full_url(shortened_link)		
-	end
+  def get_full_url(shortened_link:)
+    self.recover_full_url(shortened_link)   
+  end
 
 
-	private
-		# Total: 26 + 26 + 10 = 62 chars
-		CHAR_TO_NUMBER_DICT = [*('a'..'z'), *('A'..'Z'), *('0'..'9')]
-												.each_with_index
-												.to_h
-		NUMBER_TO_CHAR_DICT = CHAR_TO_NUMBER_DICT.invert	
-		
-		# update database
-		def update_link_with_shortened_link
-			self.shortened_link = generate_shortened_link
-			save
-		end
+  private
+    # Total: 26 + 26 + 10 = 62 chars
+    CHAR_TO_NUMBER_DICT = [*('a'..'z'), *('A'..'Z'), *('0'..'9')]
+                        .each_with_index
+                        .to_h
+    NUMBER_TO_CHAR_DICT = CHAR_TO_NUMBER_DICT.invert  
+    
+    # update database
+    def update_link_with_shortened_link
+      self.shortened_link = generate_shortened_link
+      save
+    end
 
-		# in case user didn't input http:// or https://
-		def smart_add_url_protocol
-		  unless self.full_link[/\Ahttp:\/\//] || self.full_link[/\Ahttps:\/\//]
-		    self.full_link = "http://#{self.full_link}"
-		  end
-		end
+    # in case user didn't input http:// or https://
+    def smart_add_url_protocol
+      unless self.full_link[/\Ahttp:\/\//] || self.full_link[/\Ahttps:\/\//]
+        self.full_link = "http://#{self.full_link}"
+      end
+    end
 
-		# https://stackoverflow.com/questions/742013/how-do-i-create-a-url-shortener
-		def generate_shortened_link
-			generated = ""
-			n = self.id
-			while n != 0
-				m = n % 62
-				generated << NUMBER_TO_CHAR_DICT[m]
-				n = n / 62
-			end
-			return generated.reverse
-		end
-		def find_shortened_link_primary_id(shortened_link)
-			primary_id = 0
-			shortened_link.reverse!
-			shortened_link.each_char.with_index do |char, i|
-				primary_id += CHAR_TO_NUMBER_DICT[char] * (62 ** i)
-			end
-			
-			return primary_id
-		end
+    # https://stackoverflow.com/questions/742013/how-do-i-create-a-url-shortener
+    def generate_shortened_link
+      generated = ""
+      n = self.id
+      while n != 0
+        m = n % 62
+        generated << NUMBER_TO_CHAR_DICT[m]
+        n = n / 62
+      end
+      return generated.reverse
+    end
+    def find_shortened_link_primary_id(shortened_link)
+      primary_id = 0
+      shortened_link.reverse!
+      shortened_link.each_char.with_index do |char, i|
+        primary_id += CHAR_TO_NUMBER_DICT[char] * (62 ** i)
+      end
+      
+      return primary_id
+    end
 
-		def recover_full_url(shortened_link)
-			link_id = self.find_shortened_link_primary_id(shortened_link)
-			link = Link.find_by(id: link_id)
-			return link.full_link
-		end
+    def recover_full_url(shortened_link)
+      link_id = self.find_shortened_link_primary_id(shortened_link)
+      link = Link.find_by(id: link_id)
+      return link.full_link
+    end
 end
